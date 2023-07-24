23fadf54c0fa78ff265388cfd06fbec319266bee
Akas_Datta <012akasdatta@gmail.com>
2023-07-24 21:34:43 +0600

    college data find

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cv3dsgq.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+let collegeCollection;
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+
     await client.connect();
-    // Send a ping to confirm a successful connection
+    collegeCollection = client.db('campusGo').collection('college');
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
 }
+
 run().catch(console.dir);
 
+app.get('/college', async (req, res) => {
+  try {
+    const cursor = collegeCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching colleges:", error);
+    res.status(500).json({ error: "Failed to fetch colleges" });
+  }
+});
 
 app.get('/', (req, res) => {
-    res.send('campus is running')
-})
-
-
+  res.send('campus is running');
+});
 
 app.listen(port, () => {
-    console.log(`Campus API is running on port: ${port}`)
-})
+  console.log(`Campus API is running on port: ${port}`);
+});
